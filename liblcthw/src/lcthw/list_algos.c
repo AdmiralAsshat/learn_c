1a7504da9746beed9247219474497c9feee5e208
AdmiralAsshat <asshatadmiral@gmail.com>
2013-10-05 22:25:07 -0400

    What the hell is Zed doing with strcmp?

--- liblcthw/src/lcthw/list_algos.c
+++ liblcthw/src/lcthw/list_algos.c
@@ -1,19 +1,26 @@
-List *List_sort(List *list)
+#include<lcthw/dbg.h>
+#include<lcthw/list_algos.h>
+
+List *List_bubble_sort(List *list)
 {
 	ListNode *cur;
+	int swapped = 0;
+
 	do
 	{
-		bool swapped = FALSE;
-		for (cur = list->first; cur->next != NULL; cur = cur->next;)
+		swapped = 0;
+		for (cur = list->first; cur->next != NULL; cur = cur->next)
 		{
 			if (cur->value > cur->next->value)
 			{
 				cur->next = cur->next->next;
-				cur->next->previous = cur->previous;
-				cur->previous = cur->next;
-				cur->previoux->next = cur;
-				swapped = TRUE;
+				cur->next->prev = cur->prev;
+				cur->prev = cur->next;
+				cur->prev->next = cur;
+				swapped = 1;
 			}
 		}
-	} while (swapped = FALSE);
+	} while (swapped == 0);
+
+return list;
 }
