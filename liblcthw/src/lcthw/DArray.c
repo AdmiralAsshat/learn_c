9395927ccd6ebfbf9333ec16b20751c5adfa1900
AdmiralAsshat <asshatadmiral@gmail.com>
2013-12-24 18:24:06 -0600

    Realloc is causing some memory errors.

--- liblcthw/src/lcthw/DArray.c
+++ liblcthw/src/lcthw/DArray.c
@@ -3,7 +3,10 @@
 DArray *DArray_create(size_t element_size, size_t initial_max)
 {
 	DArray *array = malloc(sizeof(DArray));
+	check_mem(array);
 	array->max = initial_max;
+	check(array->max > 0, "You must set an initial max > 0.");
+
 	array->end = 0;
 	array->element_size = element_size;
 	array->expand_rate = DEFAULT_EXPAND_RATE;
@@ -41,29 +44,43 @@ void DArray_clear(DArray *array)
 	}
 }
 
+static inline int DArray_resize(DArray *array, size_t newsize)
+{
+	array->max = newsize;
+	check(array -> max > 0, "The newsize must be > 0");
+
+	void *contents = realloc(array->contents, array->max * sizeof(void *));
+	// check contents and assume realloc doesn't harm the original on error
+
+	check_mem(contents);
+
+	array->contents = contents;
+
+	return 0;
+error:
+	return -1;
+}
 
 int DArray_expand(DArray *array)
 {
+	size_t old_max = array->max;
+	check(DArray_resize(array, array->max + array->expand_rate) == 0,
+		"Failed to expand array to new size: %d",
+		array->max + (int)array->expand_rate);
 
-	if(array->end > array->max)
-	{
-		array->max += array->expand_rate;
-	}
-
+	memset(array->contents + old_max, 0, array->expand_rate + 1);
 	return 0;
 
 error:
-	return 1;
+	return -1;
 }
 
 
 int DArray_contract(DArray *array)
 {
-	if(array->max > array->end)
-	{
-		array->max = array->end;
-	}
-	return 0;
+	int new_size = array->end < (int)array->expand_rate ? (int)array->expand_rate : array->end;
+
+	return DArray_resize(array, new_size + 1);
 
 error:
 	return 1;
@@ -90,8 +107,20 @@ error:
 
 void *DArray_pop(DArray *array)
 {
-	DArray_remove(array, array->end);
+	check(array->end -1 >= 0, "Attempt to pop from empty array.");
+
+	void *el = DArray_remove(array, array->end - 1);
 	array->end--;
+
+	if(DArray_end(array) > (int)array->expand_rate && DArray_end(array) % array->expand_rate) 
+	{
+	DArray_contract(array);
+	}
+
+	return el;
+
+error:
+	return NULL;
 }
 
 void DArray_clear_destroy(DArray *array)
