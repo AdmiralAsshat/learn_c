920aac932e6d7700a6047a340bb521d8f76f6e96
AdmiralAsshat <asshatadmiral@gmail.com>
2014-02-04 23:32:42 -0500

    Heapsort now works.

--- liblcthw/src/lcthw/darray_algos.c
+++ liblcthw/src/lcthw/darray_algos.c
@@ -1,6 +1,9 @@
 #include <lcthw/darray_algos.h>
 #include <stdlib.h>
 
+//TODO: Work on mergesort, change all heapsort functions
+// to use proper compare fn pointers
+
 int DArray_qsort(DArray *array, DArray_compare cmp)
 {
 	qsort(array->contents, DArray_count(array), sizeof(void *), cmp);
@@ -17,11 +20,11 @@ int DArray_mergesort(DArray *array, DArray_compare cmp)
 	return mergesort(array->contents, DArray_count(array), sizeof(void *), cmp);
 }
 
-inline void DArray_swap(void *a, void *b)
+inline void DArray_swap(void **a, void **b)
 {
-	void *temp = a;
-	a = b;
-	b = temp;
+	void *temp = *a;
+	*a = *b;
+	*b = temp;
 }
 
 int mergesort(void *array, int length, int size, DArray_compare cmp)
@@ -43,20 +46,19 @@ error:
 
 int siftDown(void **a, int start, int end)
 {
-	int root = start;
+	int root = start;	
 	while((root * 2) + 1 <= end)
 	{
 		int child = (root * 2) + 1;
 		int to_swap = root;
-//		if (a[to_swap] < a[child])
-		if (strcmp(a[to_swap], a[child]) < 1)
+
+		if (strcmp(a[to_swap], a[child]) < 0)
 			to_swap = child;
-//		if (child+1 <= end && a[to_swap] < a[child + 1])
-		if (child+1 <= end && strcmp(a[to_swap], a[child + 1]) < 1)
+		if (child+1 <= end && strcmp(a[to_swap], a[child + 1]) < 0)
 			to_swap = child + 1;
 		if (to_swap != root)
 		{
-			DArray_swap(a[root], a[to_swap]);
+			DArray_swap(&a[root], &a[to_swap]);
 			root = to_swap;
 		} else
 			return 0;
@@ -69,11 +71,12 @@ int heapsort(void **a, int count, int size, DArray_compare cmp)
 {
 	unsigned int end;
 	heapify(a, count);
+
 	for (end = count - 1; end > 0; end--)
 	{
-		DArray_swap(a[end], a[0]);
-		end--;
-		siftDown(a, 0, end);
+		DArray_swap(&a[end], &a[0]);
+		/*end--;*/
+		siftDown(a, 0, end - 1);
 	} 
 
 	return 0;
